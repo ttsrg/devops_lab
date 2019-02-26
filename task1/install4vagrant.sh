c1344c613eaeab03195a9267f6f483d5ad7f6fb8
ttsrg <ttserg@gmail.com>
2019-02-26 16:41:33 +0300

    return changed install.sh and added script for vagrant

--- task1/install4vagrant.sh
+++ task1/install4vagrant.sh
@@ -3,21 +3,23 @@
 
 sudo yum install -y nano mc git
 
+sudo -u vagrant -s --  <<EOF
+
 PYTHONVER=3.7.1
 
-if [[ ! -d $HOME/.pyenv/versions/$PYTHONVER ]] ; then
-        echo -en "\033[35m python $PYTHONVER  not exists \033[0m\n"
+if [[ ! -d \$HOME/.pyenv/versions/\$PYTHONVER ]] ; then
+        echo -en "\033[35m python \$PYTHONVER    not exists \033[0m\n"
         sudo yum install -y curl zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel \
         openssl-devel xz xz-devel libffi-devel 1>/dev/null
 	curl -L  https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash 1>/dev/null
 
 
-        export PATH="$HOME/.pyenv/bin:$PATH"
-        eval "$(pyenv init -)"
-        eval "$(pyenv virtualenv-init -)"
+        export PATH="\$HOME/.pyenv/bin:\$PATH"
+        eval "\$(pyenv init -)"
+        eval "\$(pyenv virtualenv-init -)"
 
-        pyenv install -f  $PYTHONVER 
-        pyenv global  $PYTHONVER 
+        pyenv install -f  \$PYTHONVER 
+        pyenv global  \$PYTHONVER 
         pip install --upgrade pip
 	pip install wheel
         pip install virtualenv
@@ -35,60 +37,57 @@ fi
 
 PYTHONVER=2.7.15
 
-if [[ ! -d $HOME/.pyenv/versions/$PYTHONVER ]] ; then
-        echo -en "\033[35m python $PYTHONVER    not exists \033[0m\n"
+if [[ ! -d \$HOME/.pyenv/versions/\$PYTHONVER ]] ; then
+        echo -en "\033[35m python \$PYTHONVER    not exists \033[0m\n"
         sudo yum install -y curl zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite$
         openssl-devel xz xz-devel libffi-devel 1>/dev/null
         curl -L  https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | $
 
 
-        export PATH="$HOME/.pyenv/bin:$PATH"
-        eval "$(pyenv init -)"
-        eval "$(pyenv virtualenv-init -)"
+        export PATH="\$HOME/.pyenv/bin:\$PATH"
+        eval "\$(pyenv init -)"
+        eval "\$(pyenv virtualenv-init -)"
 
-        pyenv install -f  $PYTHONVER
-#        pyenv global  $PYTHONVER
+        pyenv install -f  \$PYTHONVER
+#        pyenv global  \$PYTHONVER
         pip install --upgrade pip
         pip install wheel
         pip install virtualenv
         pyenv virtualenv -f virtenv2
 
-elif [[ ! -L $HOME/.pyenv/versions/virtenv2 ]] ; then
+elif [[ ! -L \$HOME/.pyenv/versions/virtenv2 ]] ; then
         echo -en "\033[35m virtualenv2  not exists \033[0m\n"
 
         pyenv virtualenv -f virtenv2
         python -V
 
 else
-    	echo -en "\033[32m Python $PYTHONVER  and virtualenv2 exists \033[0m\n"
+    	echo -en "\033[32m Python \$PYTHONVER  and virtualenv2 exists \033[0m\n"
 fi
 
 python -V
 pyenv virtualenvs
 pyenv versions
+EOF
+
 
 #workaround
-grep pyenv $HOME/.bashrc
+grep pyenv /home/vagrant/.bashrc
 if [ "$?" != "0" ]; then
     echo nopyenvEXPORT
-cat << EOF >> $HOME/.bashrc
-export PATH="$HOME/.pyenv/bin:$PATH"
+sudo -u vagrant -s --  <<EOF2
+cat << EOF >> \$HOME/.bashrc
+export PATH="\$HOME/.pyenv/bin:\$PATH"
 EOF
-    echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
-    echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
-  
+EOF2
+    echo "eval \"\$(pyenv init -)\"" >> /home/vagrant/.bashrc
+    echo "eval \"\$(pyenv virtualenv-init -)\"" >> /home/vagrant/.bashrc
 else
     echo EXPORTis
 fi
 
-source $HOME/.bashrc
-
-python -V
-pyenv virtualenvs
-pyenv versions
-
-
-
 exit 0
 
-        
+
+
+
